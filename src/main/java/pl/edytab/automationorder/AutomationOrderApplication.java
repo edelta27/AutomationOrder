63c4cb707b6f8dd6d6bdbd2234ba3e263f5bd2b2
Edyta <edysiaborowska27@gmail.com>
2025-06-11 19:27:46 +0200

    first commit

---
+++ src/main/java/pl/edytab/automationorder/AutomationOrderApplication.java
@@ -0,0 +1,13 @@
+package pl.edytab.automationorder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutomationOrderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AutomationOrderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/edytab/automationorder/AutomationOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.edytab.automationorder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutomationOrderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
